87ff7e3f7e222950db19b79aa237e264680695ef
Tom Leaman <tom@tomleaman.co.uk>
2016-02-22 11:29:43 +0000

    Confirmed day guests can now edit dietary reqs
    fixes #1

---
+++ wedding_site/static/js/rsvp.js
@@ -0,0 +1,5 @@
+$(function() {
+    $('#dietary-requirements-modal-submit-button').on('click', function() {
+        $('#update-dietary-requirements-form').submit();
+    });
+});
--- wedding_site/views/frontend.py
+++ wedding_site/views/frontend.py
@@ -5,6 +5,7 @@ from flask.ext.login import login_required
 
 from wedding_site import db
 from wedding_site.auth import OAuthSignIn
+from wedding_site.forms import DietaryRequirementsForm
 from wedding_site.models import *
 from wedding_site.models import constants
 
@@ -34,7 +35,24 @@ def user():
     if user.invitation is None or user.invitation.status == constants.NOT_INVITED:
         return render_template('not_invited.html')
     else:
-        return render_template('rsvp.html', user=user)
+        dietary_requirements_form = DietaryRequirementsForm()
+        dietary_requirements_form.dietary_requirements.data = user.dietary_requirements
+        return render_template('rsvp.html', user=user,
+                               dietary_requirements_form=dietary_requirements_form)
+
+
+@frontend.route('/update_dietary_requirements', methods=['POST'])
+def update_dietary_requirements():
+    user = current_user
+    form = DietaryRequirementsForm(request.form)
+    if not form.validate():
+        flash('Something went wrong', 'danger')
+        return redirect(url_for('.user'))
+    user.dietary_requirements = form.dietary_requirements.data
+    db.session.add(user)
+    db.session.commit()
+    flash('Your dietary requirements have been updated successfully', 'success')
+    return redirect(url_for('.user'))
 
 
 @frontend.route('/me/decline')
