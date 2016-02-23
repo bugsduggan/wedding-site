fbaf662320e481433142ac83fb5cbaa2a070a2e9
Tom Leaman <tom@tomleaman.co.uk>
2016-02-23 16:42:08 +0000

    Remove sqlalchemy-migrate rubbish

--- db_create.py
+++ db_create.py
@@ -2,27 +2,16 @@
 
 import os.path
 
-from migrate.versioning import api
-
 from wedding_site import create_app, db
 from wedding_site.models import *
 
 
 app = create_app()
 SQLALCHEMY_DATABASE_URI = app.config['SQLALCHEMY_DATABASE_URI']
-SQLALCHEMY_MIGRATE_REPO = app.config['SQLALCHEMY_MIGRATE_REPO']
 
 
 with app.app_context():
     db.create_all()
-    if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
-        api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
-        api.version_control(SQLALCHEMY_DATABASE_URI,
-                            SQLALCHEMY_MIGRATE_REPO)
-    else:
-        api.version_control(SQLALCHEMY_DATABASE_URI,
-                            SQLALCHEMY_MIGRATE_REPO,
-                            api.version(SQLALCHEMY_MIGRATE_REPO))
 
     admin_group = Group(group_name='admins', special=True)
     db.session.add(admin_group)
