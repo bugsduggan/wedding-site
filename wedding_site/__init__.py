41839f8e37b605b5576f02e87cfd985415563827
Tom Leaman <tom@tomleaman.co.uk>
2016-02-29 16:10:38 +0000

    Tidy up main file

--- wedding_site/__init__.py
+++ wedding_site/__init__.py
@@ -13,6 +13,32 @@ db = SQLAlchemy()
 lm = LoginManager()
 
 
+def load_models():
+    from wedding_site.models import user, group, invitation, todo
+
+
+load_models()
+
+
+def init_extensions(app):
+    db.init_app(app)
+
+    lm.init_app(app)
+    lm.login_view = 'frontend.oauth_authorize'
+    lm.login_message = None
+
+    Bootstrap(app)
+
+
+def init_views(app):
+    from wedding_site.views.frontend import frontend
+    app.register_blueprint(frontend)
+    from wedding_site.views.admin import admin
+    app.register_blueprint(admin)
+    from wedding_site.views.api import api
+    app.register_blueprint(api)
+
+
 def create_app(config=None):
     app = Flask(__name__)
 
@@ -23,24 +49,10 @@ def create_app(config=None):
         app.config.from_object(config)
 
     app.logger.setLevel(app.config['LOG_LEVEL'])
-
     app.json_encoder = CustomJSONEncoder
-
-    db.init_app(app)
-
-    lm.init_app(app)
-    lm.login_view = 'frontend.oauth_authorize'
-    lm.login_message = None
-
-    Bootstrap(app)
-
     app.jinja_env.globals['momentjs'] = momentjs
 
-    from wedding_site.views.frontend import frontend
-    app.register_blueprint(frontend)
-    from wedding_site.views.admin import admin
-    app.register_blueprint(admin)
-    from wedding_site.views.api import api
-    app.register_blueprint(api)
+    init_extensions(app)
+    init_views(app)
 
     return app
